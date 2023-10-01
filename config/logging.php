8ffb5fd62dd34ef1d265ee9d1074c5f6c034b5c1
PC-SHIMABUKURO\Developer <shimabukuronobuhiko@gmail.com>
2023-10-01 19:14:30 +0900

    no message

--- config/logging.php
+++ config/logging.php
@@ -65,12 +65,20 @@ return [
             'replace_placeholders' => true,
         ],
 
+        // 'daily' => [
+        //     'driver' => 'daily',
+        //     'path' => storage_path('logs/laravel.log'),
+        //     'level' => env('LOG_LEVEL', 'debug'),
+        //     'days' => 14,
+        //     'replace_placeholders' => true,
+        // ],
+
         'daily' => [
             'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
+            // 'path' => storage_path('logs/laravel.log'),
+            'path' => storage_path('logs/2/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
             'days' => 14,
-            'replace_placeholders' => true,
         ],
 
         'slack' => [
