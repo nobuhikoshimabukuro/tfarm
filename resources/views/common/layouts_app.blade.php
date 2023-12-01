9807eab8bf09b98ba47a0bf7f2dd9c0b76eb96bc
nobuhikoshimabukuro <shimabukuronobuhiko@gmail.com>
2023-12-01 10:26:35 +0900

    a
    a

--- resources/views/common/layouts_app.blade.php
+++ resources/views/common/layouts_app.blade.php
@@ -1,5 +1,5 @@
 @php 
-    $VERSION = "?" . env('VERSION');
+    $system_version = "?system_version=" . env('system_version');
 @endphp
 
 <!doctype html>
@@ -7,10 +7,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="{{ asset('css/all.css') . $VERSION}}" rel="stylesheet">
-    <link href="{{ asset('css/bootstrap.css') . $VERSION }}" rel="stylesheet">
-    <link href="{{ asset('css/style.css') . $VERSION }}" rel="stylesheet">
-    <link href="{{ asset('css/header.css') . $VERSION }}" rel="stylesheet">    
+    <link href="{{ asset('css/all.css') . $system_version}}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.css') . $system_version }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') . $system_version }}" rel="stylesheet">
+    <link href="{{ asset('css/header.css') . $system_version }}" rel="stylesheet">    
     
     <link rel="shortcut icon" href="{{ asset('img/logo/logo.ico')}}" type="image/x-icon">
     <link rel="apple-touch-icon" href="{{ asset('img/logo/logo.png')}}" sizes="180x180">
@@ -355,11 +355,11 @@
 
 
 
-<script src="{{ asset('js/bootstrap.js') }}"></script>
-<script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
-<script src="{{ asset('js/app.js') }}"></script>
-<script src="{{ asset('js/common.js') }}"></script>
-<script src="{{ asset('js/fontawesome.js') }}"></script>
+<script src="{{ asset('js/bootstrap.js') . $system_version}}"></script>
+<script src="{{ asset('js/jquery-3.6.0.min.js') . $system_version}}"></script>
+<script src="{{ asset('js/app.js') . $system_version}}"></script>
+<script src="{{ asset('js/common.js') . $system_version}}"></script>
+<script src="{{ asset('js/fontawesome.js') . $system_version}}"></script>
 
 
 <!--▽▽jQuery▽▽-->
